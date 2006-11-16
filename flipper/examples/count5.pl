12010d1b39fc124596b7d9161e4cb5ad9d6f1aff
rognes <rognes>
2006-11-16 14:46:14 +0000

    added for testing

---
+++ flipper/examples/count5.pl
@@ -0,0 +1,16 @@
+%for testing
+eee one(x) &
+eee two(x) &
+aaa -(one(x) & two(x)) &
+eee three(x) &
+aaa -(one(x) & three(x)) &
+aaa -(two(x) & three(x)) &
+eee four(x) &
+aaa -(one(x) & four(x)) &
+aaa -(two(x) & four(x)) &
+aaa -(three(x) & four(x)) &
+eee five(x) &
+aaa -(one(x) & five(x)) &
+aaa -(two(x) & five(x)) &
+aaa -(three(x) & five(x)) &
+aaa -(four(x) & five(x)).
