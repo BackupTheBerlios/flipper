2228980ad7b14b05386a94f6027f8bd11d604468
rognes <rognes>
2007-01-24 23:58:20 +0000

    radius added

--- flipper/includes/parameters.h
+++ flipper/includes/parameters.h
@@ -2,9 +2,10 @@
 #define MAX_REL_NAME 256
 #define INITIAL_RANDOM_SEED 0 // 0 means the number of seconds since 1.1.1970 will  be used
 #define MAX_TRIES 20          // initial number of tries before starting over
-#define MAX_TRIES_FACTOR 1    // rate of increase in MAX_TRIES for each iteration 
+#define MAX_TRIES_FACTOR 1    // rate of increase in MAX_TRIES for each iteration
 #define GRANULARITY 0
 #define FLIP_FACTOR 3         
+#define RADIUS 2
 #define SHOW_PROGRESS 1
 #define DESCRIBE_MODEL 1
 //the following are currently unused
